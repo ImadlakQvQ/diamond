da7681fbef3503e536e7f5772eaea0478b301ceb
ImadlakQvQ <472974582@qq.com>
2025-01-01 16:43:49 -0500

    .

---
+++ job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=def-bboulet     # set account
+#SBATCH --gpus-per-node=2         # Number of GPU(s) per node
+#SBATCH --output=log/%j.out      # log 保存地址
+#SBATCH --cpus-per-task=3         # CPU cores/threads
+#SBATCH --mem=4000M               # memory per node
+#SBATCH --time=3-02:01
+
+module load python/3.10.13 gcc cuda opencv/4.10.0
+source env/bin/activate
+export HYDRA_FULL_ERROR=1
+export PYTHONPATH=/home/imadlak/projects/def-bboulet/imadlak/program/diamond:$PYTHONPATH
+python src/main.py env.train.id=BreakoutNoFrameskip-v4 common.devices=0
