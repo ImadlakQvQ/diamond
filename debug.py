e2edaf45a2741b5bbc7d38cd455f9f3f968a3914
ImadlakQvQ <imadlak@foxmail.com>
2025-01-13 16:34:21 -0500

    .

--- debug.py
+++ debug.py
@@ -4,10 +4,7 @@ import os
 
 os.chdir('/home/xz/program/diamond')
 sys.path.append('/home/xz/program/diamond/src')
-# args = 'python -m lilab.multiview_scripts_new.s2_matpkl2ballpkl /mnt/liying.cibr.ac.cn_Data_Temp/multiview-large/TPH2KOxWT/2022-06-16ball.matpkl --time 1 9 17 23 27'
-# args = 'python -m lilab.metric_seg.s3_cocopkl_vs_cocopkl --gt_pkls /home/liying_lab/chenxinfeng/DATA/CBNetV2/data/rats_metric/te1/intense_pannel.cocopkl --pred_pkls /home/liying_lab/chenxinfeng/DATA/CBNetV2/data/rats_metric/te2/intense_pannel.cocopkl '
-# args = 'python -m lilab.multiview_scripts_dev.p2_calibpkl_refine_byglobal 2023-05-05_22-51-09Sball.calibpkl 2023-05-05_22-51-09Sball.globalrefpkl'
-# args = 'python test.py 5 7'
+
 args = 'python src/main.py --config-name debug env.train.id=BreakoutNoFrameskip-v4 common.devices=0'
 args = args.split()
 if args[0] == 'python':
